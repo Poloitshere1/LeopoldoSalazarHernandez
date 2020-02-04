5d487022fed48e52773f5407762614575e163ede
SalazarHernandez <60489937+Poloitshere1@users.noreply.github.com>
2020-02-04 09:23:41 -0800

    Create AgReObt.cpp

---
+++ AgReObt.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+int a=120;
+
+char b, c, d;
+
+if (a<90)
+{
+    printf("El angulo es agudo");
+}
+else if (a==90)
+{
+    printf("El angulo es recto");
+}
+if (a>90)
+{
+    printf("El angulo es obtuso");
+}
+}
